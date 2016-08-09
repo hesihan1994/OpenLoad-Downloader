0f5bd33e735cdf58b3cf5485e238531c69777809
Giuseppe Eletto <Majestcx@users.noreply.github.com>
2016-08-09 18:32:19 +0200

    v1.0.0
    First and (I think) last release of this script.
    Good coding ;-)

---
+++ app.php
@@ -0,0 +1,48 @@
+<?php
+require "./vendor/autoload.php";
+use JonnyW\PhantomJs\Client;
+
+$video = $_GET['id'];
+if($video == ''){
+	echo "Specificare l'ID del video.";
+	exit();
+}else{
+	$client = Client::getInstance();
+
+	if(strpos($video, '.') !== False){
+		$video = explode('.', $video)[0];
+	}
+
+	$request = $client->getMessageFactory()->createRequest("https://openload.co/embed/$video", 'GET');
+	$response = $client->getMessageFactory()->createResponse();
+	$client->send($request, $response);
+
+	if($response->getStatus() === 200) {
+		$openload = $response->getContent();
+		if(strpos($openload, 'We are sorry!') !== False){
+			echo json_encode(array('error' => '404', 'msg' => 'File not found'));
+			exit();
+		}
+	    $openload = explode('<span id="streamurl">', $openload)[1];
+	    $file = 'https://openload.co/stream/'.explode('</span>', $openload)[0].'?mime=true';
+    	$headers = get_headers($file,1);
+    	$filename = explode('?', end(explode('/',$headers['Location'])))[0];
+	   	$file = explode('?', $headers['Location'])[0];
+	   	$size = $headers['Content-Length'];
+	   	set_time_limit(0);
+	   	
+	   	header('Content-Type: video/mp4');
+		header('Content-Length: '.$size);
+	   	
+	   	$f = fopen($file, "rb");
+	   	while (!feof($f)) {
+		 	echo fread($f, 8*1024);
+		   	flush();
+		   	ob_flush();
+	   	}
+	   	exit();
+	}else{
+		echo json_encode(array('error' => $response->getStatus(), 'msg' => 'Server error'));
+		exit();
+	}
+}
